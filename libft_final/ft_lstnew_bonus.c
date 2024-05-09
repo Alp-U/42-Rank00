bf90ec35aa6069a1f4daa2ca453a2bd4dba6bb80
Alp Utku <autku@c3r3p4.42vienna.com>
2024-05-09 16:16:34 +0200

    completed libft

---
+++ libft_final/ft_lstnew_bonus.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: autku <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 16:40:03 by autku             #+#    #+#             */
+/*   Updated: 2024/04/13 12:26:25 by autku            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
+/*
+int	main(void)
+{
+	int data;
+
+	data = 42;
+	printf("%d", *(int *)(ft_lstnew(&data)->content));
+	return (0);
+}*/
